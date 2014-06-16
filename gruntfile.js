e1a5b65ddab501a0b5c919c79d6ae1e345626138
Folkert Schram de Jong <folkert.schramdejong@capgemini.com>
2014-06-16 20:22:39 +0200

    Set up basic files

---
+++ gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+
+	require('corpapp-grunt')(grunt);
+	
+	grunt.registerTask('default', [
+		'jshint:all', 
+		'concat:all',
+		'concat_css:all',
+		'connect',
+		'watch'
+	]);
+};
