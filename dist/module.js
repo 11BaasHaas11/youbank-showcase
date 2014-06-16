5a39c0966665d9ae350160715ad2d05b8d3e736f
Folkert Schram de Jong <folkert.schramdejong@capgemini.com>
2014-06-16 20:39:20 +0200

    Add index.html

---
+++ dist/module.js
@@ -0,0 +1,16 @@
+angular.module('youbankApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
